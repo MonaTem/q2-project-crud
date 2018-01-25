4f6fc533f5f24822fde7fe584d1872757b7fca3f
Mona Temchin <monatemchin@gmail.com>
2018-01-25 12:05:56 -0500

    Fix bad knexfile adr in db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const APP_MODE = process.env.APP_MODE;
-const config   = require('../knexfile')[APP_MODE];
+const config   = require('./knexfile')[APP_MODE];
 const knex     = require('knex')(config);
 
 module.exports = knex;
